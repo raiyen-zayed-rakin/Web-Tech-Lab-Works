db6fde021847cb2a243a4909d790e0876e7fffa9
Raiyen Zayed Rakin <rakinaiub2023@gmail.com>
2025-03-13 09:13:02 +0600

    Task1

---
+++ view/customer_registration.php
@@ -0,0 +1,36 @@
+<h1>Customer Registration</h1>
+<fieldset>
+    <legend><label for="">Personal Details</label></legend>
+    <form action="">
+        <label for="fname">First Name:</label><br>
+        <input type="text" placeholder="Enter first name"><br>
+        <label for="lname">Last Name:</label><br>
+        <input type="text" placeholder="Enter last name"><br>
+        <label for="nname">Nickname(optional):</label><br>
+        <input type="text" placeholder="Enter first name"><br>
+        <label for="">Email Address:</label><br>
+        <input type="text" placeholder="Enter email address"><br>
+        <label for="">Phone Number:</label><br>
+        <input type="text" placeholder="Enter email address"><br>
+        <label for="">Date of Birth:</label>
+        <input type="date" name="" id=""><br>
+        <label for="">Gender:</label>
+        <input type="radio" id="gender1" name="gender1" value="gender1">Male
+        <input type="radio" id="gender2" name="gender1" value="gender2">Female
+        <input type="radio" id="gender2" name="gender1" value="gender2">other<br>
+        <label for="">Current Address:</label><br>
+        <input type="text" placeholder="Enter address">
+    </form>
+</fieldset>
+<fieldset>
+    <legend><label for="">User Details</label></legend>
+    <form action="">
+        <label for="fname">Create Username:</label><br>
+        <input type="text" placeholder="Enter username"><br>
+        <label for="">Own or work in a restaurant business:</label>
+        <input type="radio" id="yes" name="yes" value="yes">yes
+        <input type="radio" id="no" name="yes" value="no">No
+    </form>
+</fieldset><br>
+<input type="button" value="Create Account" name="" id="">
+<input type="button" value="Reset Fields" name="" id="">
